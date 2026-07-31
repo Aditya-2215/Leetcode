b4652cee84f5da804f3d4d42552db11e5202ed14
Aditya Raj Pandey <2k23.psitaiml2311215@gmail.com>
2026-07-31 22:21:25 +0530

    Time: 914 ms (5.07%), Space: 328 MB (7.06%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,9 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = float('inf')
-        max_profit = 0
-        for price in prices:
-            min_price = min(min_price, price)
-            profit = price - min_price
-            max_profit = max(max_profit, profit)
-        return max_profit
+        n=len(prices)
+        memo={}
+        def solve(index,buy,tran):
+            #Base Case
+            if index==n or tran==0:
+                return 0
+            if (index,buy,tran) in memo:
+                return memo[(index,buy,tran)]
+            if buy:
+                buyStock=-prices[index]+solve(index+1,False,tran)
+                skip=solve(index+1,True,tran)
+                ans=max(buyStock,skip)
+            else:
+                sellStock=prices[index]+solve(index+1,True,tran-1)
+                skip=solve(index+1,False,tran)
+                ans=max(sellStock,skip)
+            memo[(index,buy,tran)]=ans
+            return ans
+        return solve(0,True,1)
