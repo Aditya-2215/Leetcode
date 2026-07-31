26d43f809326623d10b210f60dbcab927eeaf7da
Aditya Raj Pandey <2k23.psitaiml2311215@gmail.com>
2026-07-31 12:50:43 +0530

    Time: 55 ms (5.01%), Space: 40 MB (5.01%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -3,11 +3,14 @@ class Solution:
         n=len(prices)
         memo={}
         def solve(index,buy):
+            #Base Case
             if index==n:
                 return 0
+            #key for memoization
             key=(index,buy)
             if key in memo:
                 return memo[key]
+
             if buy:
                 buy_Stocks=-prices[index]+solve(index+1,False)
                 skip=solve(index+1,True)
@@ -18,4 +21,5 @@ class Solution:
                 skip=solve(index+1,False)
                 memo[key]=max(sell_Stock,skip)
                 return memo[key]
+        #Driver code
         return solve(0,True)
