f89a97b60bbbcd36c6a9bb5b08f863cf158717db
Aditya Raj Pandey <2k23.psitaiml2311215@gmail.com>
2026-08-01 12:20:47 +0530

    Time: 1720 ms (7.5%), Space: 505.6 MB (5.02%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,30 +1,27 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        n=len(prices)
-        #memo={}
-        @cache
-        def solve(index,buy,tran):
-            #Base Case
-            if index==n:
+        n = len(prices)
+        memo = {}
+        
+        def solve(index, buy, tran):
+            # Base Case
+            if index == n or tran == 0:
                 return 0
-            if tran==0:
-                return 0
-           # key=(index,buy,tran)
-           # if key in memo:
-           #     return memo[key]
-            #if person buys the stock and Not Buy it then we use here buys and skip respectively 
+                
+            key = (index, buy, tran)
+            if key in memo:
+                return memo[key]
+            
             if buy:
-                buys=-prices[index]+solve(index+1,False,tran)
-                skip=solve(index+1,True,tran)
-                return max(buys,skip)
-              #  memo[key]=max(buys,skip)
-                # return memo[key]
-            #For buying the second stock we need to first sell the previous stock
+                buys = -prices[index] + solve(index + 1, False, tran)
+                skip = solve(index + 1, True, tran)
+                memo[key] = max(buys, skip)
             else:
-                sell=prices[index]+solve(index+1,True,tran-1)
-                skip=solve(index+1,False,tran)
-                return max(sell,skip)
-                # memo[key]=max(sell,skip)
-                # return memo[key]
-        #Driver Code
-        return solve(0,True,2)
+                sell = prices[index] + solve(index + 1, True, tran - 1)
+                skip = solve(index + 1, False, tran)
+                memo[key] = max(sell, skip)
+                
+            return memo[key]
+            
+        # Driver Code
+        return solve(0, True, 2)
