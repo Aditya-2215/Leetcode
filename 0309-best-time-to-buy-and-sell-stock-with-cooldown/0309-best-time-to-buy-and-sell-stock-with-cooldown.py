6e135492f1a6981440ada291ae218f271228fbbc
Aditya Raj Pandey <2k23.psitaiml2311215@gmail.com>
2026-08-01 12:10:15 +0530

    Time: 6 ms (29.15%), Space: 21.3 MB (17.35%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -2,19 +2,22 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n=len(prices)
         #@cache #if we use this then it will automatically reduce the size of the TC from O(2^n) to O(n)
-        @cache
+        memo={}
         def solve(i,flag):
             if i>=n:
                 return 0
+            if (i,flag) in memo:
+                return memo[(i,flag)]
             if flag:
                 sell = +prices[i]+solve(i+2,0)
                 N_sell=solve(i+1,1)
                 ans=max(sell,N_sell)
-                return ans
+                
             else:
                 buy=-prices[i]+solve(i+1,1)
                 N_buy=solve(i+1,0)
                 ans=max(buy,N_buy)
-                return ans
+            memo[(i,flag)]=ans
+            return ans
                 
         return solve(0,0)
