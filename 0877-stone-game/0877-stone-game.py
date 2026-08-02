aaa8a2d52790e777599c8c23cfbf9d0e3048c9de
Aditya Raj Pandey <2k23.psitaiml2311215@gmail.com>
2026-08-02 13:58:42 +0530

    Time: 346 ms (26.26%), Space: 137.9 MB (14.53%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,15 +1,17 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
         n=len(piles)
-        memo={}
+        #memo={}
+        @cache
         def solve(i,j):
             if i==j:
                 return piles[i]
-            if (i,j) in memo:
-                return memo[(i,j)]
+            #if (i,j) in memo:
+             #   return memo[(i,j)]
 
             takeLeft=piles[i]-solve(i+1,j)
             takeRight=piles[j]-solve(i,j-1)
-            memo[(i,j)]=max(takeLeft,takeRight)
+           # memo[(i,j)]=max(takeLeft,takeRight)
+            return max(takeLeft,takeRight)
             return memo[(i,j)]
         return solve(0,n-1)>0
