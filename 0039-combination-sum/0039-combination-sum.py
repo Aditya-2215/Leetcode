e7c32eb51f6fef3f37430540ab983ed65d65c26e
Aditya Raj Pandey <2k23.psitaiml2311215@gmail.com>
2026-08-08 14:38:02 +0530

    Time: 7 ms (76.57%), Space: 19.7 MB (8.96%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -3,7 +3,7 @@ class Solution:
         n=len(candidates)
         result=[]
         path=[]
-        def backtrack(start,current_sum):
+        def solve(start,current_sum):
             if current_sum==target:
                 result.append(path.copy())
                 return
@@ -11,7 +11,7 @@ class Solution:
                 return
             for i in range(start,n):
                 path.append(candidates[i])
-                backtrack(i,current_sum+candidates[i])
+                solve(i,current_sum+candidates[i])
                 path.pop()
-        backtrack(0,0)
+        solve(0,0)
         return result
