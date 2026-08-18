b43faa3c4ed5cc46e0380943a4152dfce82e0d75
Aditya Raj Pandey <2k23.psitaiml2311215@gmail.com>
2026-08-18 23:34:14 +0530

    Time: 808 ms (58.11%), Space: 0B (100%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,5 +1,5 @@
 SELECT p.product_id,
-       NVL(ROUND(SUM(p.price * u.units) / SUM(u.units),2),0) AS average_price
+NVL(ROUND(SUM(p.price * u.units) / SUM(u.units),2),0) AS average_price
 FROM Prices p
 LEFT JOIN UnitsSold u
     ON p.product_id = u.product_id
