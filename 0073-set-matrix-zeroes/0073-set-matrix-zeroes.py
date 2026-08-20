3dcdaacb79b38aea9b0de45cdcf2ceedb70499cc
Aditya Raj Pandey <2k23.psitaiml2311215@gmail.com>
2026-08-20 15:06:45 +0530

    Time: 1 ms (94.16%), Space: 20.8 MB (42.78%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,14 +1,34 @@
 class Solution:
-    def setZeroes(self, matrix):
-        m = len(matrix)
-        n = len(matrix[0])
-        zeros = []
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        n=len(matrix)
+        m=len(matrix[0])
+        FR=False
+        FC=False
         for i in range(m):
-            for j in range(n):
-                if matrix[i][j] == 0:
-                    zeros.append((i, j))
-        for r, c in zeros:
-            for j in range(n):
-                matrix[r][j] = 0
-            for i in range(m):
-                matrix[i][c] = 0
+            if matrix[0][i]==0:
+                FR=True
+        for j in range(n):
+            if matrix[j][0]==0:
+                FC=True
+        for i in range(1,n):
+            for j in range(1,m):
+                if matrix[i][j]==0:
+                    matrix[i][0]=0
+                    matrix[0][j]=0
+        col0=0
+        for j in range(1,m):
+            if matrix[0][j]==0:
+                for i in range(1,n):
+                    matrix[i][j]=0
+        for i in range(1,n):
+            if matrix[i][0]==0:
+                for j in range(1,m):
+                    matrix[i][j]=0
+        if FR:
+            for j in range(m):
+                matrix[0][j]=0
+                
+        
+        if FC:
+            for i in range(n):
+                matrix[i][0]=0
