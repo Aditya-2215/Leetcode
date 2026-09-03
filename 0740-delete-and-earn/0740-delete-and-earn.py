4e1b50fb142f469841e349e7a45ebfb9fb3b8f33
Aditya Raj Pandey <2k23.psitaiml2311215@gmail.com>
2026-09-03 15:07:19 +0530

    Time: 16 ms (11.93%), Space: 24.3 MB (8.91%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -0,0 +1,18 @@
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        count=Counter(nums)
+        max_num=max(nums) 
+        memo={}
+        def solve(i):
+            if i>max_num:
+                return 0
+            if i in memo:
+                return memo[i]
+            skip=solve(i+1)
+            take=i*count[i]+solve(i+2)
+            memo[i]= max(skip,take)
+            return memo[i]
+        return solve(1)
+        
+    
+
